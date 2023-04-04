8acf0b7e58f552c01105764c822266e6baca48f7
Ubuntu <linuxuser@ubuntu.ckv4rbtgqcpejmrwenw5j15ksa.rx.internal.cloudapp.net>
2023-04-04 07:02:34 +0000

    terraform to create windows Virutal machine

--- terraform-01/main.tf
+++ terraform-01/main.tf
@@ -5,9 +5,66 @@ terraform {
             version = "~> 3.0.0"
         }
     }
+
     required_version=">=0.15.0"
 }
 
-provider = "azurerm" {
+provider  "azurerm" {
     features{}
+}
+
+resource "azurerm_resource_group" "main" {
+    name = "first-tf-rg-centralindia"
+    location = "centralindia"
+}
+
+resource "azurerm_virtual_network" "main" {
+   name ="first-tf-vnet-centralindia"
+   location = azurerm_resource_group.main.location
+   resource_group_name = azurerm_resource_group.main.name
+   address_space = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "main" {
+  name = "first-tf-subnet-centralindia"
+  virtual_network_name = azurerm_virtual_network.main.name
+  resource_group_name = azurerm_resource_group.main.name
+  address_prefixes = ["10.0.0.0/24"]
+}
+
+resource "azurerm_network_interface" "internal" {
+ name = "first-tf-nic-internal-centralindia"
+ location = azurerm_resource_group.main.location
+ resource_group_name = azurerm_resource_group.main.name
+ ip_configuration {
+   name = "internal"
+   subnet_id = azurerm_subnet.main.id
+   private_ip_address_allocation = "Dynamic"
+ }
+}
+
+resource "azurerm_windows_virtual_machine" "main" {
+  name = "first-tf-vm-in"
+  resource_group_name = azurerm_resource_group.main.name
+  location = azurerm_resource_group.main.location
+  size = "Standard_B2s"
+  admin_username = "user.admin"
+  admin_password = ""
+
+  network_interface_ids = [
+     azurerm_network_interface.internal.id
+  ]
+  
+  os_disk {
+   caching = "ReadWrite"
+   storage_account_type = "Standard_LRS"
+  }
+
+ source_image_reference {
+   publisher = "MicrosoftwindowsServer"
+   offer = "WindowsServer"
+   sku = "2016-DataCenter"
+   version = "latest"
+ }
+ 
 }
